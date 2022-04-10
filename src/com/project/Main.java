c788580add9a1c538293ed55c6ea63d5529d6a4e
Roman <61121402+rebrov-rc@users.noreply.github.com>
2022-04-10 19:33:03 +0300

    homework final

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -1,8 +1,30 @@
 package com.project;
 
+import stream.StreamMain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Main {
 
     public static void main(String[] args) {
 
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> res = new ArrayList<>();
+        for (Integer i: intList ) {
+            if (i > 0 && i%2 == 0){
+                res.add(i);
+            }
+        }
+        res.sort((a,b) -> a - b);
+
+
+        System.out.println(res.toString());
+
+        // By Stream API
+        StreamMain.main(intList);
     }
 }
---
+++ src/stream/StreamMain.java
@@ -0,0 +1,17 @@
+package stream;
+
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMain {
+
+    public static void main(List<Integer> intList){
+        Stream<Integer> stream = intList.stream();
+        stream.filter(x -> x > 0)
+                .filter(x -> x%2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+    }
+}
